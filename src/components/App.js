d1c89d771d1d1e24276c12adb29e643f53b2f426
Claire <claire.cy.t@gmail.com>
2021-10-21 11:47:29 -0400

    add theme file

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,13 @@
 import React from "react";
 import Header from "../components/ui/Header";
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './ui/Theme'
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      {[...new Array(200)]
-            .map(
-              () => `Cras mattis consectetur purus sit amet fermentum.
-Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
-            )
-            .join('\n')}
-    </div>
+    <ThemeProvider theme={theme}>
+      <Header /> 
+    </ThemeProvider>
   );
 }
 
