f73a47273a526e370b4e800d231cea6308a1a922
Claire <claire.cy.t@gmail.com>
2021-10-23 20:28:10 -0400

    set default value

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Box from "@mui/material/Box";
@@ -9,7 +9,7 @@ import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import Button from "@mui/material/Button";
 import { styled } from "@mui/material/styles";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 import logo from "../../assets/logo.svg";
 
@@ -30,16 +30,16 @@ const ColorButton = styled(Button)(({ theme }) => ({
   borderRadius: "50px",
   marginLeft: "50px",
   marginRight: "25px",
-  height:'45px',
-  ...theme.typography.estimate
+  height: "45px",
+  ...theme.typography.estimate,
 }));
 const StyledTab = styled(Tab)(({ theme }) => ({
   ...theme.typography.tab,
   minWidth: "10 ",
   marginLeft: "25px",
-  color: 'rgba(255, 255, 255, 0.7)',
-  '&.Mui-selected': {
-    color: '#fff',
+  color: "rgba(255, 255, 255, 0.7)",
+  "&.Mui-selected": {
+    color: "#fff",
   },
 }));
 
@@ -61,10 +61,27 @@ ElevationScroll.propTypes = {
 
 export default function Header(props) {
   const classes = useStyles();
-  const [value, setValue] = React.useState(1);
+  const [value, setValue] = React.useState(0);
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
+
+  useEffect(() => {
+    if (window.location.pathname === "/" && value !== 0) {
+      setValue(0);
+    } else if (window.location.pathname === "/services" && value !== 1) {
+      setValue(1);
+    } else if (window.location.pathname === "/revolution" && value !== 2) {
+      setValue(2);
+    } else if (window.location.pathname === "/about" && value !== 3) {
+      setValue(3);
+    }else if (window.location.pathname === "/contact" && value !== 4) {
+      setValue(4);
+    }else if (window.location.pathname === "/estimate" && value !== 5) {
+      setValue(5);
+    }
+  },[value]);
+
   return (
     <React.Fragment>
       <Box sx={{ flexGrow: 1 }}>
@@ -78,11 +95,36 @@ export default function Header(props) {
                 className={classes.tabContainer}
                 onChange={handleChange}
               >
-                <StyledTab label="Home" className={classes.tab} component={Link} to='/'/>
-                <StyledTab label="Services" className={classes.tab} component={Link} to='/services'/>
-                <StyledTab label="The Revolution" className={classes.tab} component={Link} to='/revolution'/>
-                <StyledTab label="About Us" className={classes.tab} component={Link} to='/about' />
-                <StyledTab label="Contact Us" className={classes.tab} component={Link} to='/contact'/>
+                <StyledTab
+                  label="Home"
+                  className={classes.tab}
+                  component={Link}
+                  to="/"
+                />
+                <StyledTab
+                  label="Services"
+                  className={classes.tab}
+                  component={Link}
+                  to="/services"
+                />
+                <StyledTab
+                  label="The Revolution"
+                  className={classes.tab}
+                  component={Link}
+                  to="/revolution"
+                />
+                <StyledTab
+                  label="About Us"
+                  className={classes.tab}
+                  component={Link}
+                  to="/about"
+                />
+                <StyledTab
+                  label="Contact Us"
+                  className={classes.tab}
+                  component={Link}
+                  to="/contact"
+                />
               </Tabs>
               <ColorButton
                 variant="contained"
